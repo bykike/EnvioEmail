ae949fe4a96c98cc7860351182613aa757f68305
Kike <kike@areavisual.net>
2017-05-31 09:44:52 +0200

    Nuevas formas de envío
    Mandar email de diversas maneras.

--- envioemail.php
+++ envioemail.php
@@ -1,10 +1,52 @@
 <?php
 
+	$to = "destinatario@dominio.es";
+    $subject = "Testeando mail";
+    $message = "Hello! This is a simple email message.";
+    $from = "remitente@dominio.es";
+    $headers = "From:" . $from;
+
+	mail($to, $subject, $message, $headers);
+    echo "Mail Sent to destinatario@dominio.es.";
+    exit("Fin");
+
+
+	// Opción 3
+
+	$mail = "Prueba de mensaje";
+	//Titulo
+	$titulo = "Título";
+	//cabecera
+	$headers = "MIME-Version: 1.0\r\n"; 
+	$headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+	//dirección del remitente 
+	$headers .= "From: Geeky < remitente@dominio.es >\r\n";
+	//Enviamos el mensaje a tu_dirección_email 
+	$bool = mail("destinatario@dominio.es",$titulo,$mail,$headers);
+	if($bool){
+	    echo "Mensaje enviado";
+		}else{
+		    echo "Mensaje no enviado";
+		}
+
+
+
+	// Opción 2	
+
+	$to = "destinatario@dominio.es, destinatario2@dominio.es, destinatario3@dominio.es";
+	$subject = "Asunto del email";
+	$message = "Envío de email con PHP";
+	$headers = "From: remitente@dominio.es" . "\r\n" . "CC: remitente2@dominio.es";
+	 
+	mail($to, $subject, $message, $headers);
+
+
+
+	// Opción 1	
+
 	// Género un randomke
 	$random_key = generate_random_key();
 	$validated = 0;
-
-
 	$email = "kodeispoetry@gmail.com";
 	// Enviar un mensaje para confirmar la dirección de email introducida
 	mail($email,"Activación de la cuenta",
@@ -15,7 +57,7 @@
 				     http://activate.php?activation=".$random_key);
 
 
-
+	
 	function generate_random_key() {
 	    $chars = "abcdefghijklmnopqrstuvwxyz0123456789";
 	 
@@ -24,4 +66,6 @@
 	        $new_key .= $chars[rand(0,35)];
 	    }
 	    return $new_key;
-}
+	} 
+
+?>
